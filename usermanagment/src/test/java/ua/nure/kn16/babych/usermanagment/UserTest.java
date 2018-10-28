016959e6a596ff26bfb11c0f1d4d519df3817214
Yaroslav Babych <yaroslav.babych@nure.ua>
2018-10-28 18:04:43 +0200

    Description for tests

--- usermanagment/src/test/java/ua/nure/kn16/babych/usermanagment/UserTest.java
+++ usermanagment/src/test/java/ua/nure/kn16/babych/usermanagment/UserTest.java
@@ -16,12 +16,19 @@ public class UserTest {
 
     public static int currentDay;
 
+
+    // Test if string concatenation works
     @Test
     public void getFullNameTest() {
         User user = new User(ID, FIRST_NAME, LAST_NAME, null);
         assertEquals("Иванов, Иван", user.getFullName());
     }
 
+    // These tests were written in October of 2018
+    // Some of them may break in the future
+
+    // Test getAge
+    // when birthday was earlier this year
     @Test
     public void simpleAgeTestAfter(){
         Calendar calendar = Calendar.getInstance();
@@ -30,6 +37,8 @@ public class UserTest {
         assertEquals(19, user.getAge());
     }
 
+    // Test getAge
+    // when birthday is coming later this year
     @Test
     public void simpleAgeTestBefore(){
         Calendar calendar = Calendar.getInstance();
@@ -38,6 +47,8 @@ public class UserTest {
         assertEquals(18, user.getAge());
     }
 
+    // Test getAge
+    // when birthday was earlier this month
     @Test
     public void ageTestSameMonthAfter(){
         Calendar calendar = Calendar.getInstance();
@@ -46,6 +57,8 @@ public class UserTest {
         assertEquals(19, user.getAge());
     }
 
+    // Test getAge
+    // when birthday is coming later this month
     @Test
     public void ageTestSameMonthBefore(){
         Calendar calendar = Calendar.getInstance();
@@ -62,6 +75,8 @@ public class UserTest {
         currentDay = calendar.get(Calendar.DATE);
     }
 
+    // Test getAge
+    // when birthday is today
     @Test
     public void ageTestSameDay(){
         Calendar calendar = Calendar.getInstance();
